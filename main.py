390b2ea917297cf3ba5eee8376698d8f7a86af92
Sllo7 <120136660+Sloo7ee@users.noreply.github.com>
2023-06-03 18:22:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-import pygame
-
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
